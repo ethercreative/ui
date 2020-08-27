2da07c42c72fba30c279bad3ad87d141ac269d13
James Edmonston <james@ethercreative.co.uk>
2020-08-27 17:21:51 +0100

    Fix column export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { default as Column } from './Container';
+export { default as Column } from './Column';
 export { default as Container } from './Container';
 export { default as Grid } from './Grid';
 export { default as Image } from './Image';
