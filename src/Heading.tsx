570eace40e8b7469013b64aee5236e2a5f5792bf
James Edmonston <james@ethercreative.co.uk>
2020-09-01 12:36:26 +0100

    Fix heading levels type

--- src/Heading.tsx
+++ src/Heading.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 
-const LEVELS = ['1', '2', '3', '4', '5', '6'];
+const LEVELS = {
+  '1': '1',
+  '2': '2',
+  '3': '3',
+  '4': '4',
+  '5': '5',
+  '6': '6',
+};
 
 const SIZES = {
   xs: 'text-xs',
