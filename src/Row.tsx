c36057cdb24b4e416d8e16989c2df7b93ab940bd
James Edmonston <james@ethercreative.co.uk>
2020-09-01 10:16:37 +0100

    Add row

---
+++ src/Row.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+const SPAN = {
+  '1': 'row-span-1',
+  '2': 'row-span-2',
+  '3': 'row-span-3',
+  '4': 'row-span-4',
+  '5': 'row-span-5',
+  '6': 'row-span-6',
+};
+
+const START = {
+  '1': 'row-start-1',
+  '2': 'row-start-2',
+  '3': 'row-start-3',
+  '4': 'row-start-4',
+  '5': 'row-start-5',
+  '6': 'row-start-6',
+  '7': 'row-start-7',
+};
+
+interface Props {
+  span?: keyof typeof SPAN;
+  start?: keyof typeof START;
+  className?: React.HTMLAttributes<HTMLDivElement>['className'];
+  style?: React.HTMLAttributes<HTMLDivElement>['style'];
+}
+
+const Row: React.FC<Props> = ({ span, start, className, children }) => {
+  let computedClass = '';
+
+  if (span) {
+    computedClass += ` ${SPAN[span]}`;
+  }
+
+  if (start) {
+    computedClass += ` ${START[start]}`;
+  }
+
+  return <div className={`${computedClass} ${className}`}>{children}</div>;
+};
+
+export default Row;
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { default as Column } from './Column';
 export { default as Container } from './Container';
 export { default as Grid } from './Grid';
 export { default as Image } from './Image';
+export { default as Row } from './Row';
 export { default as Scroll } from './Scroll';
 export { default as Spacing } from './Spacing';
