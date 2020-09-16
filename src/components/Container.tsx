430bb7bcdf247c7bcd90556bde41323e0fd47ec4
James Edmonston <james@ethercreative.co.uk>
2020-09-16 11:46:58 +0100

    Reduce mobile container margin

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const Container: React.FC = ({ children }) => (
-  <div className='xl:container w-full mx-auto px-10'>{children}</div>
+  <div className='xl:container w-full mx-auto px-6 md:px-10'>{children}</div>
 );
 
 export default Container;
